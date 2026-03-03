65898288c5cf767883aabd91256fc94d67a54ada
Son Nguyen <nhuson2306@gmail.com>
2026-03-03 18:38:01 +0700

    chore: Comment projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Home from '@/app/components/home';
 import About from '@/app/components/about';
 import Experience from '@/app/components/experience';
 import Skills from '@/app/components/skill';
-import Projects from '@/app/components/project';
+// import Projects from '@/app/components/project';
 import Contact from '@/app/components/contact';
 import Footer from '@/app/components/footer';
 import { CurrentThemeProvider } from '@/app/context/current-theme.context';
@@ -19,7 +19,7 @@ const HomePage = () => {
         <About />
         <Experience />
         <Skills />
-        <Projects />
+        {/* <Projects /> */}
         <Contact />
         <Footer />
       </CurrentThemeProvider>
